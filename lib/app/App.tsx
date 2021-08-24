a0a6e4774f0c81a5baf5c4ef24dbecabcbc7a851
JPeer264 <jan.oster94@gmail.com>
2021-08-24 11:28:27 +0200

    Feat: create chunks for views

--- lib/app/App.tsx
+++ lib/app/App.tsx
@@ -1,3 +1,4 @@
+import loadable from '@loadable/component';
 import { FC } from 'react';
 import { DehydratedState } from 'react-query/types/hydration';
 import { Route, Switch } from 'react-router-dom';
@@ -6,9 +7,9 @@ import Header from './components/Header';
 import Helmet from './components/Helmet';
 import GlobalStyles from './GlobalStyles';
 
-import Home from './views/Home';
-import UserDetail from './views/UserDetail';
-import UsersOverview from './views/UsersOverview';
+const Home = loadable(() => import('./views/Home'));
+const UserDetail = loadable(() => import('./views/UserDetail'));
+const UsersOverview = loadable(() => import('./views/UsersOverview'));
 
 export interface PreloadedData {
   query?: DehydratedState;
